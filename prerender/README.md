f02521ed3e5b94dd8806b9c678b2d2776ce932b6
Jan Maghuyop <jan.maghuyop.ph@gmail.com>
2025-04-07 17:13:24 +0800

    prerender: create dockerfile and compose

--- prerender/README.md
+++ prerender/README.md
@@ -1,3 +1,15 @@
 # Prerender
 - https://github.com/prerender/prerender
 - https://github.com/prerender/prerender-node
+- https://github.com/tvanro/prerender-alpine
+- https://hub.docker.com/r/tvanro/prerender-alpine
+## Usage
+```bash
+git clone git@github.com:prerender/prerender.git
+cd prerender
+rm -frv .git* test .pre* LICENSE
+docker-compose build prerender
+docker-compose up
+# test
+curl http://localhost:3000/render?url=https://www.example.com/
+```
---
+++ prerender/prerender/server.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+var prerender = require('./lib');
+
+// https://github.com/tvanro/prerender-alpine/blob/master/server.js
+const server = prerender({
+    chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars', '--disable-dev-shm-usage'],
+    forwardHeaders: true,
+    chromeLocation: '/usr/bin/chromium-browser'
+});
+
+server.use(prerender.sendPrerenderHeader());
+server.use(prerender.browserForceRestart());
+// server.use(prerender.blockResources());
+server.use(prerender.addMetaTags());
+server.use(prerender.removeScriptTags());
+server.use(prerender.httpHeaders());
+
+server.start();
